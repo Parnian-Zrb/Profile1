64afebe75bd4097fbcc9e75b27cb64c873d98808
Parnian Zarbafian <parnian.zarbafian@gmail.com>
2024-03-05 11:51:58 -0500

    form

--- components/Form.tsx
+++ components/Form.tsx
@@ -1,41 +1,49 @@
 import "./Form.css";
 import { useState } from "react";
 
+// interface FormProps {
+//   name: string;
+//   email: string;
+//   message: string;
+//   handleClick: () => void;
+// }
 const Form = () => {
   const [name, setName] = useState<string>("");
   const [email, setEmail] = useState<string>("");
   const [message, setMessage] = useState<string>("");
+  const handleClick = () => {
+    alert("Thank you for sumitting the form");
+  };
 
   return (
     <div>
-      <form action="">
-        <input type="text" value={name} placeholder="Name" />
-        <input type="email" value={email} placeholder="Email" />
-        <textarea
+      <form>
+        <input
+          type="text"
+          value={name}
+          placeholder="Name"
+          onChange={(e) => setName(e.target.value)}
+        />
+        <br />
+        <input
+          type="text"
+          value={email}
+          placeholder="Email"
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <br />
+        <input
+          type="text"
           value={message}
-          placeholder="Type your message here"
-        ></textarea>
+          placeholder="Type Your  Message Here..."
+          onChange={(e) => setMessage(e.target.value)}
+        />
+        <br />
 
-        <button type="submit">Submit</button>
+        <button onClick={handleClick}> Submit</button>
       </form>
     </div>
   );
 };
 
 export default Form;
-// const Form = () => {
-//   return (
-//     <div>
-//       <form className="input">
-//         <input type="text" placeholder="your name" /> <br />
-//         <input type="Email" placeholder="your Email" />
-//         <br />
-//         <textarea placeholder=" Type your message Here" />
-//         <br />
-//         <button> Send </button>
-//       </form>
-//     </div>
-//   );
-// };
-
-// export default Form;
